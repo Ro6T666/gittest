9448e9714e89b87148f197d013abfd68b9586443
unknown <root@atguigu.com>
2022-04-25 13:56:42 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Auther:Administrator
+ * @Date:2022/4/25 0025 13:22
+ * @Description:
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
